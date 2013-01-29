2aa615e2caf2f25c423cb6a11fc98d2a9eb8e6fd
Promotos <promotos@gmx.de>
2013-01-29 22:04:04 +0100

    add some user tests.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "simple create user" do
+     user = User.create(user_id: 'unit', password: 'test', password_confirmation: 'test')
+     assert user.persisted?
+   end
+   
+   test "simple create user and delete" do
+     user = User.create(user_id: 'unit', password: 'test', password_confirmation: 'test')
+     assert user.persisted?
+     
+     user.destroy
+     assert !user.persisted?
+   end
+   
+   test "simple create user authenticate" do
+     user = User.create(user_id: 'unit', password: 'test', password_confirmation: 'test')
+     assert user.persisted?
+     
+     current = User.authenticate('unit', 'test')
+     assert user.valid?, 'Check if the user instance is provided by the authenticate method'
+   end
 end

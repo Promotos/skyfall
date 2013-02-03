610d324ef3a41b01559983a5bc459e045972c952
Promotos <promotos@gmx.de>
2013-02-03 18:08:28 +0100

    correct test due not logged on.

--- test/functional/notes_controller_test.rb
+++ test/functional/notes_controller_test.rb
@@ -3,27 +3,27 @@ require 'test_helper'
 class NotesControllerTest < ActionController::TestCase
   test "should get list" do
     get :list
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get create" do
     get :create
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get show" do
     get :show
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get edit" do
     get :edit
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get destroy" do
     get :destroy
-    assert_response :success
+    assert_response :redirect
   end
 
 end
--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SettingsControllerTest < ActionController::TestCase
   test "should get list" do
     get :list
-    assert_response :success
+    assert_response :redirect
   end
 
 end

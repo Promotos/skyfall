74d6625b053990c3d6a6eab6092239a276044c00
Promotos <promotos@gmx.de>
2013-02-09 21:47:08 +0100

    Show action for notes.

--- README.md
+++ README.md
@@ -2,7 +2,3 @@ skyfall
 =======
 
 Used as personal cloud.
-
-
-rake db:seed RAILS_ENV="production"
-rake db:migrate RAILS_ENV="production"
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -17,6 +17,7 @@ class NotesController < ApplicationController
   end
 
   def show
+    load_user_by_id
   end
 
   def edit
@@ -24,4 +25,15 @@ class NotesController < ApplicationController
 
   def destroy
   end
+
+  private 
+  def load_user_by_id
+    if params.has_key?(:id)
+      @note = Note.find(params[:id])
+      redirect_to notes_list_path, :alert => "Note not found." unless @note
+    else
+      redirect_to notes_list_path, :alert => "Parameter :id is missing."
+    end
+  end
+
 end

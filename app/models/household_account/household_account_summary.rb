6ec82a14776c59f569779b24a0af548b9f8c7da5
Promotos <promotos@gmx.de>
2013-03-09 22:08:40 +0100

    Add payment chart

--- app/models/household_account/household_account_summary.rb
+++ app/models/household_account/household_account_summary.rb
@@ -23,6 +23,17 @@ class HouseholdAccountSummary
 		return ('%.2f' % sum).gsub('.', ',')
 	end
 
+	def get_summary_for_chart (month, category)
+		@payments = HouseholdAccountPayment.where('household_account_category_id = ? AND payment_date BETWEEN ? AND ?', 
+			category.id, month.beginning_of_month, month.end_of_month)
+		return "0" if @payments.empty?
+		sum = 0
+		@payments.each { |payment|
+			sum += payment.sum
+		}
+		return sum.to_f
+	end	
+
 	def get_summary (month, category)
 		payments = HouseholdAccountPayment.where('household_account_category_id = ? AND payment_date BETWEEN ? AND ?', 
 			category.id, month.beginning_of_month, month.end_of_month)
